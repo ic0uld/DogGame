fda206521cd47ec52ade6652650668df41e3281c
ic0uld <ic0uld81@gmail.com>
2023-10-29 18:31:46 +0300

    IK Improvement

--- Plugins/AdvancedSessions/Intermediate/Build/Win64/UnrealEditor/Development/AdvancedSessions/Definitions.AdvancedSessions.h
+++ Plugins/AdvancedSessions/Intermediate/Build/Win64/UnrealEditor/Development/AdvancedSessions/Definitions.AdvancedSessions.h
@@ -64,9 +64,9 @@
 #define UE_BUILD_DEVELOPMENT 1
 #define UE_IS_ENGINE_MODULE 0
 #define UE_DISABLE_INLINE_GEN_CPP 0
-#define UE_ENABLE_INCLUDE_ORDER_DEPRECATED_IN_5_1 0
-#define UE_PROJECT_NAME VRExpPluginExample
-#define UE_TARGET_NAME VRExpPluginExampleEditor
+#define UE_ENABLE_INCLUDE_ORDER_DEPRECATED_IN_5_1 1
+#define UE_PROJECT_NAME DogsClub
+#define UE_TARGET_NAME CommandoHeroEditor
 #define WITH_ADVANCED_SESSIONS 1
 #define UE_MODULE_NAME "AdvancedSessions"
 #define UE_PLUGIN_NAME "AdvancedSessions"
